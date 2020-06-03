0875e9e0c3e67bbd60f1a2558568f01b73a6d755
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2020-06-03 08:54:18 -0300

    Improve run_test.sh
    If any of the two processes in the pipe fail, the whole test fails.

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,9 +1,9 @@
 # DO NOT CALL THIS SCRIPT DIRECTLY.
 # Run `meson test` on the build dir.
 
-unset EMILUA_COLORS
-
 AWKSCRIPT="$1"
 TEST="$2"
+
+set -o pipefail
 
 "$EMILUA_BIN" "$TEST.lua" 2>&1 | "$AWK_BIN" -v TEST="$TEST" -f "$AWKSCRIPT"
