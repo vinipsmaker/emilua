19ae075863cc1cd96bcd2147d59e937ab41e2023
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2020-12-07 04:11:27 -0300

    Load table module during VM startup

--- test/actor22.lua
+++ test/actor22.lua
@@ -9,13 +9,8 @@ else assert(_CONTEXT == 'worker')
     local m = inbox:recv()
 
     if getmetatable(m[1]) == 'tx-channel' then
-        local m2 = {}
-        for i, v in ipairs(m) do
-            if i > 1 then
-                m2[i - 1] = v
-            end
-        end
-        m[1]:send(m2)
+        local ch = table.remove(m, 1)
+        ch:send(m)
     else
         local json = require('json')
         print(json.encode(m))
