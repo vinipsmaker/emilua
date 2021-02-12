f2aed36c7df0b9a81d99a2c1af60ae95762b52e9
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2021-02-12 11:58:56 -0300

    Add missing pragma once to headers

--- include/emilua/plugin.hpp
+++ include/emilua/plugin.hpp
@@ -3,6 +3,8 @@
    Distributed under the Boost Software License, Version 1.0. (See accompanying
    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
 
+#pragma once
+
 #include <emilua/core.hpp>
 
 #include <boost/preprocessor/list/cat.hpp>
