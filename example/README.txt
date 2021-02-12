c5d9e98da07b72923affd08592c5cc1224859505
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2021-02-12 11:53:06 -0300

    Add native plugin example

--- example/README.txt
+++ example/README.txt
@@ -1,1 +1,1 @@
-I'm releasing all Lua source code in this directory under public domain.
+I'm releasing all Lua and C++ source code in this directory under public domain.
---
+++ example/native_plugin/src/main.cpp
@@ -0,0 +1,51 @@
+#include <emilua/plugin.hpp>
+#include <iostream>
+
+namespace asio = boost::asio;
+
+class my_plugin : emilua::plugin
+{
+public:
+    my_plugin()
+    {
+        std::cout << "Plugin loaded\n" << std::flush;
+    }
+
+    ~my_plugin() override
+    {
+        std::cout << "Plugin unloaded\n" << std::flush;
+    }
+
+    void init_appctx(emilua::app_context&) noexcept override
+    {
+        std::cout << "Plugin initialized\n" << std::flush;
+    }
+
+    void init_ioctx_services(asio::io_context&) noexcept override
+    {
+        std::cout <<
+            "Plugin (re-)installed io services into current execution"
+            " context\n" <<
+            std::flush;
+    }
+
+    std::error_code init_lua_module(emilua::vm_context&, lua_State* L) override
+    {
+        std::cout << "Plugin imported\n" << std::flush;
+        lua_newtable(L);
+        lua_pushliteral(L, "hello");
+        lua_pushcfunction(L, hello);
+        lua_rawset(L, -3);
+        return {};
+    }
+
+private:
+    static int hello(lua_State*)
+    {
+        std::cout << "Hello World\n" << std::flush;
+        return 0;
+    }
+};
+
+extern "C" BOOST_SYMBOL_EXPORT my_plugin EMILUA_PLUGIN_SYMBOL;
+my_plugin EMILUA_PLUGIN_SYMBOL;
