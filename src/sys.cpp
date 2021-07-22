aaff4c81e5fee2bf087b4ee846d9e7e80af1ed8c
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2021-07-22 18:10:59 -0300

    Add sys.args
    Updates #5

---
+++ src/sys.cpp
@@ -0,0 +1,64 @@
+/* Copyright (c) 2021 Vinícius dos Santos Oliveira
+
+   Distributed under the Boost Software License, Version 1.0. (See accompanying
+   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
+
+#include <emilua/sys.hpp>
+
+#include <emilua/dispatch_table.hpp>
+
+namespace emilua {
+
+char sys_key;
+
+inline int sys_args(lua_State* L)
+{
+    auto& appctx = get_vm_context(L).appctx;
+    lua_createtable(L, /*narr=*/appctx.app_args.size(), /*nrec=*/0);
+    int n = 0;
+    for (auto& arg: appctx.app_args) {
+        push(L, arg);
+        lua_rawseti(L, -2, ++n);
+    }
+    return 1;
+}
+
+static int sys_mt_index(lua_State* L)
+{
+    return dispatch_table::dispatch(
+        hana::make_tuple(
+            hana::make_pair(BOOST_HANA_STRING("args"), sys_args)
+        ),
+        [](std::string_view /*key*/, lua_State* L) -> int {
+            push(L, errc::bad_index, "index", 2);
+            return lua_error(L);
+        },
+        tostringview(L, 2),
+        L
+    );
+}
+
+void init_sys(lua_State* L)
+{
+    lua_pushlightuserdata(L, &sys_key);
+    {
+        lua_newuserdata(L, /*size=*/1);
+
+        {
+            lua_createtable(L, /*narr=*/0, /*nrec=*/2);
+
+            lua_pushliteral(L, "__metatable");
+            lua_pushliteral(L, "sys");
+            lua_rawset(L, -3);
+
+            lua_pushliteral(L, "__index");
+            lua_pushcfunction(L, sys_mt_index);
+            lua_rawset(L, -3);
+        }
+
+        setmetatable(L, -2);
+    }
+    lua_rawset(L, LUA_REGISTRYINDEX);
+}
+
+} // namespace emilua
