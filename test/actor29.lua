7baa5b5815b477ff0b4131e07fdc4697b902a18d
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2022-04-23 07:42:33 -0300

    Rename ctx to context in spawn_ctx_threads/inherit_ctx
    * spawn_ctx_threads() is now spawn_context_threads
    * inherit_ctx is now inherit_context

--- test/actor29.lua
+++ test/actor29.lua
@@ -1,6 +1,6 @@
 if _CONTEXT == 'main' then
-    spawn_vm('.', { inherit_ctx = false, concurrency_hint = 1 })
+    spawn_vm('.', { inherit_context = false, concurrency_hint = 1 })
 else
     assert(_CONTEXT == 'worker')
-    spawn_ctx_threads(0)
+    spawn_context_threads(0)
 end
