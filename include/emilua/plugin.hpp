cabbdc6739999fc6bcdd7f4cd0c19f649a4e7e43
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2022-05-16 09:13:31 -0300

    Fix plugin.hpp indentation

--- include/emilua/plugin.hpp
+++ include/emilua/plugin.hpp
@@ -34,32 +34,32 @@ namespace emilua {
 class BOOST_SYMBOL_VISIBLE plugin
 {
 public:
-   // Called only once per `appctx`. On normal conditions there is only one
-   // `appctx` to the whole process, but the user might create custom
-   // "launchers" to his project where multiple app contexts exist.
-   virtual void init_appctx(app_context& appctx) noexcept;
+    // Called only once per `appctx`. On normal conditions there is only one
+    // `appctx` to the whole process, but the user might create custom
+    // "launchers" to his project where multiple app contexts exist.
+    virtual void init_appctx(app_context& appctx) noexcept;
 
-   // It may be called multiple times on the same `ioctx` object. Use it to
-   // register new services in the execution loop.
-   virtual std::error_code init_ioctx_services(
-       asio::io_context& ioctx) noexcept;
+    // It may be called multiple times on the same `ioctx` object. Use it to
+    // register new services in the execution loop.
+    virtual std::error_code init_ioctx_services(
+        asio::io_context& ioctx) noexcept;
 
-   // Called only once per VM. It should push the Lua module (table) on the
-   // stack on success and nothing on failure. It must only throw LUA_ERRMEM
-   // errors and no other error (use the return value to notify other error
-   // reasons).
-   //
-   // NOTE: If the function fails, it might be called again if Lua code try to
-   // import the same module again.
-   //
-   // NOTE: There is no such a thing as an "async" native plugin load (e.g. a
-   // plugin that suspends the caller module fiber). It'd be a hassle to
-   // implement that. Just apply "lazy loading" techniques on the next layer if
-   // you need such a thing (e.g. a function that checks if the module is ready
-   // and suspend until so otherwise).
-   virtual std::error_code init_lua_module(vm_context& vm_ctx, lua_State* L);
+    // Called only once per VM. It should push the Lua module (table) on the
+    // stack on success and nothing on failure. It must only throw LUA_ERRMEM
+    // errors and no other error (use the return value to notify other error
+    // reasons).
+    //
+    // NOTE: If the function fails, it might be called again if Lua code try to
+    // import the same module again.
+    //
+    // NOTE: There is no such a thing as an "async" native plugin load (e.g. a
+    // plugin that suspends the caller module fiber). It'd be a hassle to
+    // implement that. Just apply "lazy loading" techniques on the next layer if
+    // you need such a thing (e.g. a function that checks if the module is ready
+    // and suspend until so otherwise).
+    virtual std::error_code init_lua_module(vm_context& vm_ctx, lua_State* L);
 
-   virtual ~plugin() = 0;
+    virtual ~plugin() = 0;
 };
 
 inline plugin::~plugin() = default;
