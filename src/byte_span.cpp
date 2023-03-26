cd26ed213da5adc74ee64d5d586a5805c04eabd7
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2023-03-26 14:40:46 -0300

    Add missing copyright notice to src/byte_span.cpp

--- src/byte_span.cpp
+++ src/byte_span.cpp
@@ -1,3 +1,8 @@
+/* Copyright (c) 2021 Vinícius dos Santos Oliveira
+
+   Distributed under the Boost Software License, Version 1.0. (See accompanying
+   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
+
 #include <emilua/byte_span.hpp>
 
 #include <cstring>
