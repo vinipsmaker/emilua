bc5f50b2318709522549133d2e5989bd9a1c191d
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2023-03-29 10:03:31 -0300

    Quote arguments in test/run_test.bat

--- test/run_test.bat
+++ test/run_test.bat
@@ -2,15 +2,15 @@
 :: Run `meson test` on the build dir.
 @ECHO OFF
 
-MKDIR %TEMP%\emilua-test 2> NUL
-ECHO > %TEMP%\emilua-test\%~n3
+MKDIR "%TEMP%\emilua-test" 2> NUL
+ECHO > "%TEMP%\emilua-test\%~n3"
 
-( %EMILUA_BIN% %3.lua 2>&1 && DEL %TEMP%\emilua-test\%~n3 ) | %AWK_BIN% -v TEST=%3 -f %2
+( "%EMILUA_BIN%" "%3.lua" 2>&1 && DEL "%TEMP%\emilua-test\%~n3" ) | "%AWK_BIN%" -v "TEST=%3" -f "%2"
 
 IF ERRORLEVEL 1 (
     EXIT /B %ERRORLEVEL%
-) ELSE IF EXIST %TEMP%\emilua-test\%~n3 (
-    DEL %TEMP%\emilua-test\%~n3
+) ELSE IF EXIST "%TEMP%\emilua-test\%~n3" (
+    DEL "%TEMP%\emilua-test\%~n3"
     EXIT /B 2
 ) ELSE (
     EXIT /B 0
